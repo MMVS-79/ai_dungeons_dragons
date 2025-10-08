3c5059cb5bd8a26554a4a4ab7ca8ad3dd55bf732
camdnnn <cameron634@gmail.com>
2025-10-07 20:44:48 -0600

    added tables

---
+++ lib/schema/tables/logs.sql
@@ -0,0 +1,7 @@
+CREATE TABLE logs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    campaign_id INT NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (campaign_id) REFERENCES campaigns(id) ON DELETE SET NULL
+)
