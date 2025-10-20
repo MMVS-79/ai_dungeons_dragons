aae0cab0b914b03b77105113800cff4d026dbd84
MMVS <zeinny1997@gmail.com>
2025-10-20 15:52:44 -0600

    changes

--- lib/rebuild.sh
+++ lib/rebuild.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Change to the directory where this script is located
+cd "$(dirname "$0")"
+
 # Load environment variables from .env if it exists
 if [ -f ../.env ]; then
   export $(grep -v '^#' ../.env | xargs)
