a451e61e1859f87ddd6e5485af021746270da780
MMVS <zeinny1997@gmail.com>
2025-11-05 01:06:47 -0700

    revert test error

--- app/campaigns/page.tsx
+++ app/campaigns/page.tsx
@@ -9,7 +9,7 @@ interface Campaign {
   name: string;
 }
 
-export default function CampaignsPage() 
+export default function CampaignsPage() {
   const [campaigns, setCampaigns] = useState<Campaign[]>([]);
   const [nextId, setNextId] = useState(1);
 
