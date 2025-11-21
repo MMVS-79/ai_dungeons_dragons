c0786cc60f118831f675bc5af2e216a29f5b9bba
LS-Song-UC <le.song@ucalgary.ca>
2025-11-21 16:20:03 -0700

    now removed default export behavior, replaced with inplace export in function def.

--- lib/db.ts
+++ lib/db.ts
@@ -16,7 +16,7 @@
 import 'dotenv/config';
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
+export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -26,4 +26,3 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
-export default pool;
