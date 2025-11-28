4ac2d0843c20c4ee7bc4943f46229a9fdd8cbc42
MMVS <zeinny1997@gmail.com>
2025-11-28 16:51:32 -0700

    fluff ignore

--- lib/schema/tables/campaigns.sql
+++ lib/schema/tables/campaigns.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS campaigns;
-
 CREATE TABLE campaigns (
     id INT PRIMARY KEY AUTO_INCREMENT,
     account_id INT NOT NULL,
@@ -17,15 +16,11 @@ CREATE TRIGGER campaigns_limit_before_insert
 BEFORE INSERT ON campaigns
 FOR EACH ROW
 BEGIN
-    DECLARE cnt INT;
-    SELECT COUNT(*) INTO cnt
-    FROM campaigns
-    WHERE account_id = NEW.account_id;
-
-    IF cnt >= 5 THEN
-        SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Limit of 5 campaigns per account exceeded';
-    END IF;
+  DECLARE cnt INT;
+  SELECT COUNT(*) INTO cnt FROM campaigns WHERE account_id = NEW.account_id;
+  IF cnt >= 5 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Limit of 5 campaigns per account exceeded';
+  END IF;
 END $$
 
 DELIMITER ;
